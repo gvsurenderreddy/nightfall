345bf5f967767ecdb22baba8cd112c781a387911
kpcyrd <git@rxv.cc>
2015-02-12 19:31:53 +0000

    Add spaces

--- nightfall.js
+++ nightfall.js
@@ -1,32 +1,32 @@
-var nightfall={};
+var nightfall = {};
 
 // keep your peers in a bucket
-var bucket=nightfall.bucket={};
+var bucket = nightfall.bucket = {};
 // clean up every thirty seconds
-var CLEANING_INTERVAL=nightfall.CLEANING_INTERVAL=1000*30;
+var CLEANING_INTERVAL = nightfall.CLEANING_INTERVAL = 1000 * 30;
 // time out every five minutes
-var TIMEOUT=nightfall.TIMEOUT=1000*60*5;
+var TIMEOUT = nightfall.TIMEOUT = 1000 * 60 * 5;
 
 // 3 peers within a particular range
-var PEERS_FROM_RANGE=nightfall.PEERS_FROM_RANGE=3;
+var PEERS_FROM_RANGE = nightfall.PEERS_FROM_RANGE = 3;
 // keep six peers by default
-var TOTAL_PEERS=nightfall.TOTAL_PEERS=6;
+var TOTAL_PEERS = nightfall.TOTAL_PEERS = 6;
 
 // track the number of available peers
-var peers_available=nightfall.peers_available=0;
+var peers_available = nightfall.peers_available = 0;
 
 // if used as a plugin, you'll need another 
-var prefix=nightfall.prefix='/nightfall';
+var prefix = nightfall.prefix = '/nightfall';
 
-var motto=nightfall.motto=function(){/*The masses are the decisive
+var motto = nightfall.motto = function() {/*The masses are the decisive
 element, they are the rock on
 which the final victory of the
 revolution will be built.
-*/}.toString().slice(14,-3);
+*/}.toString().slice(14, -3);
 
 /* Do this every three seconds */
-var backgroundRoutine=nightfall.backgroundRoutine=function(frequency){
-  frequency=frequency||3000;
+var backgroundRoutine = nightfall.backgroundRoutine = function(frequency) {
+  frequency = frequency || 3000;
   setInterval(function() {
     for(var a in bucket) {
       for(var b in bucket[a]) {
@@ -39,44 +39,44 @@ var backgroundRoutine=nightfall.backgroundRoutine=function(frequency){
         }
       }
     }
-  },frequency);
+  }, frequency);
 };
 
 /* */
-var getRandomItem=nightfall.getRandomItem=function(list){
-  var keys=Object.keys(list);
-  return list[keys[Math.floor(Math.random()*keys.length)]];
+var getRandomItem = nightfall.getRandomItem = function(list) {
+  var keys = Object.keys(list);
+  return list[keys[Math.floor(Math.random() * keys.length)]];
 };
 
 /* */
-var addRandomPeers=nightfall.addRandomPeers=function(set,bucket,num){
-  while(set.length<num){
-    var x=getRandomItem(bucket);
-    var y=getRandomItem(x);
-    if(set.indexOf(y)<0){
+var addRandomPeers = nightfall.addRandomPeers = function(set, bucket, num) {
+  while(set.length < num) {
+    var x = getRandomItem(bucket);
+    var y = getRandomItem(x);
+    if(set.indexOf(y) < 0) {
       set.push(y);
     }
   }
   return set;
 };
 
-var main=nightfall.main=function(req,res,next){
-  next=next||function(result){
-    result=result||motto;
-    res.setHeader('Content-Type','text/plain');
+var main = nightfall.main = function(req, res, next) {
+  next = next || function(result) {
+    result = result || motto;
+    res.setHeader('Content-Type', 'text/plain');
     res.end(result);
   };
 
   // let's not assume we'll only receive an ipv4
-  var IP=req.connection.remoteAddress;
+  var IP = req.connection.remoteAddress;
   var ip;
-  if(/^\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}$/.test(IP)){
-    ip=IP.split('.');
-  }else{ // it must be an ipv6
+  if(/^\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}$/.test(IP)) {
+    ip = IP.split('.');
+  } else { // it must be an ipv6
     next('This service is meant to connect users via their ipv4 addresses.');
   }
 
-  var url=req.url.replace(prefix,''); // strip out the prefix
+  var url = req.url.replace(prefix, ''); // strip out the prefix
   var chunks = url.split('/'); // break the url into its components
   var topic = chunks[1];
   var action = chunks[2];
@@ -84,27 +84,27 @@ var main=nightfall.main=function(req,res,next){
   var range = ip[0] + '.' + ip[1];
   var host = ip[2] + '.' + ip[3];
 
-  if(action == 'seek'){
+  if(action == 'seek') {
     var set = [];
-    if(topic in bucket){
-      if(range in bucket[topic]){
+    if(topic in bucket) {
+      if(range in bucket[topic]) {
         var peers_from_range = Math.min(bucket[topic][range].length, PEERS_FROM_RANGE);
-        set=addRandomPeers([],bucket[topic][range],peers_from_range);
+        set = addRandomPeers([],bucket[topic][range], peers_from_range);
       }
-      var total_peers=Math.min(peers_available,TOTAL_PEERS);
-      set = addRandomPeers(set,bucket[topic],total_peers);
+      var total_peers = Math.min(peers_available, TOTAL_PEERS);
+      set = addRandomPeers(set, bucket[topic], total_peers);
     }
     res.end(JSON.stringify(set) + '\n');
-  }else if(action==='have'){
-    var body='';
+  } else if(action === 'have') {
+    var body = '';
 
-    req.on('data', function(data){
+    req.on('data', function(data) {
       body += data;
       if(body.length > 1e3) {
         req.connection.destroy();
       }
     }).on('end', function() {
-      var reply={};
+      var reply = {};
       var json;
       try {
         json = JSON.parse(body);
@@ -151,15 +151,15 @@ var main=nightfall.main=function(req,res,next){
 };
 
 /* If this is being called as a standalone server */
-if(require.main===module){
+if(require.main === module) {
   /* start the background process */
   backgroundRoutine();
 
   /* launch the server */
-  require("http").createServer(main).listen(7473,'', function() {
+  require('http').createServer(main).listen(7473, '', function() {
     console.log('[+] Nightfall is ready');
   });
-}else{ 
+} else { 
   /* otherwise (if it is being included as a library) */
-  module.exports=nightfall;
+  module.exports = nightfall;
 }
